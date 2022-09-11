170f2ee59bb3c6d463cca5c6f65995273e4b5d3a
tgp0315 <guiguzi0315@163.com>
2022-09-11 21:27:36 +0800

    修复读取resolve

--- utils/index.js
+++ utils/index.js
@@ -22,7 +22,7 @@ async function readBody(stream) {
 function rewriteImports(content) {
   return content.replace(/ from ['|"]([^'"]+)['|"]/g, function(s0, s1) {
 
-    if (s1[0] !== "." && s1[1] !== '/') {
+    if (s1[0] !== "." && s1[1] !== '/' && s1[0] !== '/') {
       return ` from '/@modules/${s1}'`
     } else {
       return s0
